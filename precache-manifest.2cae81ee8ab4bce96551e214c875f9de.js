06c41c6584cb09cc610808ab9c1d8f145ce9864e
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-12 09:41:14 -0600

    Updates

---
+++ precache-manifest.2cae81ee8ab4bce96551e214c875f9de.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b369dac45ecce6945b34c2c5a940bdc",
+    "url": "/RosaIsela31/PortafolioReact/index.html"
+  },
+  {
+    "revision": "7b3d25afe697d983e6b8",
+    "url": "/RosaIsela31/PortafolioReact/static/css/main.0917b22a.chunk.css"
+  },
+  {
+    "revision": "3f7ae468377ff7567df4",
+    "url": "/RosaIsela31/PortafolioReact/static/js/2.12d98b3a.chunk.js"
+  },
+  {
+    "revision": "7b3d25afe697d983e6b8",
+    "url": "/RosaIsela31/PortafolioReact/static/js/main.95fffdea.chunk.js"
+  },
+  {
+    "revision": "f3813831a61683197187",
+    "url": "/RosaIsela31/PortafolioReact/static/js/runtime-main.e0b658be.js"
+  },
+  {
+    "revision": "7e6dcca063b923e0ce2bdbd04d6d3e61",
+    "url": "/RosaIsela31/PortafolioReact/static/media/burgerqueen-project1.7e6dcca0.png"
+  },
+  {
+    "revision": "1478116d82316eaa47026557a6b78c9b",
+    "url": "/RosaIsela31/PortafolioReact/static/media/circle.1478116d.png"
+  },
+  {
+    "revision": "b1b39876ce915c53bcd1664dc83ae81e",
+    "url": "/RosaIsela31/PortafolioReact/static/media/css.b1b39876.png"
+  },
+  {
+    "revision": "0760874b4c489e3a182e9dd6fc2d68de",
+    "url": "/RosaIsela31/PortafolioReact/static/media/email.0760874b.png"
+  },
+  {
+    "revision": "54a112f28d111ece4ff6bc1d7a27ae3a",
+    "url": "/RosaIsela31/PortafolioReact/static/media/exit.54a112f2.png"
+  },
+  {
+    "revision": "bd8bc42cabbe8c91172f41a1a60a0a22",
+    "url": "/RosaIsela31/PortafolioReact/static/media/firebase.bd8bc42c.png"
+  },
+  {
+    "revision": "756dbe666c773350f80f4c80bbbfad7c",
+    "url": "/RosaIsela31/PortafolioReact/static/media/git.756dbe66.png"
+  },
+  {
+    "revision": "671ef19c4b2fc876f28ab09fb2f09de0",
+    "url": "/RosaIsela31/PortafolioReact/static/media/github1.671ef19c.png"
+  },
+  {
+    "revision": "7cd2ce12fbfb3c8360d7c9daf6faee4f",
+    "url": "/RosaIsela31/PortafolioReact/static/media/html.7cd2ce12.png"
+  },
+  {
+    "revision": "2b0fb6d0add044a9b60f6e8c31e17635",
+    "url": "/RosaIsela31/PortafolioReact/static/media/javascript.2b0fb6d0.png"
+  },
+  {
+    "revision": "16029ab20e72cbbaa09fb6b28aba56cf",
+    "url": "/RosaIsela31/PortafolioReact/static/media/labcar-project.16029ab2.png"
+  },
+  {
+    "revision": "df9991aa8524dc338bc356186b1f3f38",
+    "url": "/RosaIsela31/PortafolioReact/static/media/linkedin.df9991aa.png"
+  },
+  {
+    "revision": "5704f91af5be69fe4433b345ffac6d5e",
+    "url": "/RosaIsela31/PortafolioReact/static/media/marcaR.5704f91a.png"
+  },
+  {
+    "revision": "d6528fb0facda6747c821c7540c8d099",
+    "url": "/RosaIsela31/PortafolioReact/static/media/node1.d6528fb0.png"
+  },
+  {
+    "revision": "ae18dd9e9977dbb221395bbe7d6a113e",
+    "url": "/RosaIsela31/PortafolioReact/static/media/photo.ae18dd9e.jpg"
+  },
+  {
+    "revision": "3804c5b546b794d01e2ffabe4dd9115f",
+    "url": "/RosaIsela31/PortafolioReact/static/media/poke2.3804c5b5.png"
+  },
+  {
+    "revision": "cab24a3c419977d05f6abf7a7acb1320",
+    "url": "/RosaIsela31/PortafolioReact/static/media/react2.cab24a3c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PortafolioReact/precache-manifest.c6e6fd5bb2b86d35ca14e3af31bd8604.js"
+  "/RosaIsela31/PortafolioReact/precache-manifest.2cae81ee8ab4bce96551e214c875f9de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortafolioReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RosaIsela31/PortafolioReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
